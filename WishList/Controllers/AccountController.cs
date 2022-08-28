8f965382a692719b03469eaf422482d02cc290f0
Dhanashri Sankpal <dsankpal675@gmail.com>
2022-08-28 07:01:40 +0000

    HttpGet

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -20,5 +20,11 @@ namespace WishList.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
     }
 }
